b013dab0dee519585283cb0b756250580b67f333
irenervara <irenervara@gmail.com>
2022-03-29 19:53:28 +0200

    whole upload

--- js/scripts-sidebar - Copy.js
+++ js/scripts-sidebar - Copy.js
@@ -34,6 +34,84 @@ $(window).on('load', function() {
 */
 
 ///////////////////// MAPBOX TUTORIAL / EXAMPLE /////////////////////
+
+/*const places = {
+    'type': 'FeatureCollection',
+    'features': [{
+            'type': 'Feature',
+            'properties': {
+                'icon': 'theatre'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.038659, 38.931567]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'theatre'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.003168, 38.894651]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'bar'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.090372, 38.881189]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'bicycle'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.052477, 38.943951]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.031706, 38.914581]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.020945, 38.878241]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'music'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.007481, 38.876516]
+            }
+        }
+    ]
+};
+*/
+
+
 const places = {
   "type": "FeatureCollection",
   "name": "df_example-2",
@@ -260,185 +338,78 @@ const places = {
 const filterGroup = document.getElementById('filter-group_escala');
 const filterGroup2 = document.getElementById('filter-group_usos');
 
+/*
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-77.04, 38.907],
+    zoom: 11.15
+});
+*/
+
 map.on('load', () => {
     // Add a GeoJSON source containing place coordinates and information.
     map.addSource('places', {
         'type': 'geojson',
-        'data': 'data/prueba/df_example-2.geojson'
-        //'data': places
+        'data': places
     });
 
-    // 1. DF OBRAS (TODOS)
-    map.addLayer({
-        'id': 'points_obras',
-        'type': 'circle',
-        'source': 'places',
-        'layout': {
-            'visibility': 'visible'
-          },
-        'paint': {
-            'circle-stroke-color': '#fc03db', //pink
-            'circle-stroke-width':3,
-            'circle-opacity': 0,
-            'circle-radius': 5
-        },
-        });
-
-    // 2. FILTRADO (POR BUTTONS)
     for (const feature of places.features) {
-        const symbol_escala = feature.properties.escala_tramos;
-        const symbol_usos = feature.properties.usos;
+        const symbol = feature.properties.escala_tramos;
+        //const layerID = 'escala'
+        const layerID = symbol;
 
-        // ESCALA
         // Add a layer for this symbol type if it hasn't been added already.
-        if (!map.getLayer(symbol_escala)) {
+        if (!map.getLayer(layerID)) {
             map.addLayer({
-                'id': symbol_escala,
+                'id': layerID,
                 'type': 'circle',
                 'source': 'places',
                 'layout': {
-                    'visibility': 'none'
+                    'visibility': 'visible'
                   },
                 'paint': {
-                    'circle-color': '#fc03db',
-                    'circle-opacity': 0.5,
+                    'circle-color': '#00FFFF',
+                    'circle-opacity': 0.7,
                     'circle-radius': 5
                 },
-                'filter': ['==', 'escala_tramos', symbol_escala]
+
+                /*
+                'layout': {
+                    // These icons are a part of the Mapbox Light style.
+                    // To view all images available in a Mapbox style, open
+                    // the style in Mapbox Studio and click the "Images" tab.
+                    // To add a new image to the style at runtime see
+                    // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+
+                    'icon-image': `${symbol}-15`,
+                    'icon-allow-overlap': true
+
+                },*/
+                'filter': ['==', 'escala_tramos', symbol]
             });
             // Add checkbox and label elements for the layer.
             const input = document.createElement('input');
             input.type = 'checkbox';
-            input.id = symbol_escala;
-            input.checked = false;
+            input.id = layerID;
+            input.checked = true;
             filterGroup.appendChild(input);
 
             const label = document.createElement('label');
-            label.setAttribute('for', symbol_escala);
-            label.textContent = symbol_escala;
+            label.setAttribute('for', layerID);
+            label.textContent = symbol;
             filterGroup.appendChild(label);
 
             // When the checkbox changes, update the visibility of the layer.
             input.addEventListener('change', (e) => {
                 map.setLayoutProperty(
-                    symbol_escala,
+                    layerID,
                     'visibility',
                     e.target.checked ? 'visible' : 'none'
                 );
-                map.setLayoutProperty(
-                  symbol_usos,
-                  'visibility','none'
-                );
             });
         }
-
-        // USOS
-        // Add a layer for this symbol type if it hasn't been added already.
-        if (!map.getLayer(symbol_usos)) {
-            map.addLayer({
-                'id': symbol_usos,
-                'type': 'circle',
-                'source': 'places',
-                'layout': {
-                    'visibility': 'none'
-                  },
-                'paint': {
-                    'circle-color':'#fc03db', //yellow
-                    'circle-opacity': 0.5,
-                    'circle-radius': 10
-                },
-                'filter': ['==', 'usos', symbol_usos]
-            });
-            // Add checkbox and label elements for the layer.
-            const input_usos = document.createElement('input');
-            input_usos.type = 'checkbox';
-            input_usos.id = symbol_usos;
-            input_usos.checked = false;
-            filterGroup2.appendChild(input_usos);
-
-            const label_usos = document.createElement('label');
-            label_usos.setAttribute('for', symbol_usos);
-            label_usos.textContent = symbol_usos;
-            filterGroup2.appendChild(label_usos);
-
-            // When the checkbox changes, update the visibility of the layer.
-            input_usos.addEventListener('change', (e) => {
-                map.setLayoutProperty(
-                    symbol_usos,
-                    'visibility',
-                    e.target.checked ? 'visible' : 'none'
-                );
-                map.setLayoutProperty(
-                  symbol_escala,
-                  'visibility','none'
-                );
-            });
-        }
-
     }
 });
 
 ///////////////////// MAPBOX TUTORIAL / EXAMPLE /////////////////////
-
-
-////////////////////// POP-UP //////////////////////
-// Create a popup, but don't add it to the map yet.
-var popup = new mapboxgl.Popup({
-  closeButton: false,
-  closeOnClick: false
-});
-
-map.on('click', function (e) {
-  // query for the features under the mouse, but only in the lots layer
-  // queryrenderedFeatures looks at the layers (not the source)
-  var features = map.queryRenderedFeatures(e.point, {
-      layers: ['points_obras'],
-  });
-
-  if (features.length > 0) {
-    // show the Popup
-    // Populate the popup and set its coordinates
-    // based on the feature found
-
-    var clickedFeature = features[0]
-    var info_titulo = clickedFeature.properties.titulo_Obra
-    var info_acr = clickedFeature.properties.acr
-    var info_year = clickedFeature.properties.año
-    var info_pais = clickedFeature.properties.pais
-    var info_city = clickedFeature.properties.city
-    var info_tipoObra = clickedFeature.properties.tipo_obra
-    var info_usos = clickedFeature.properties.usos
-    var info_presu = clickedFeature.properties.presupuesto
-    var info_moneda = clickedFeature.properties.moneda
-    var info_dimension = clickedFeature.properties.escala_tramos
-    var info_autores = clickedFeature.properties.autores
-    var info_imgURL = clickedFeature.properties.img_url
-    var info_webURL = clickedFeature.properties.web_url
-
-    var popupContent =
-    `<img class='popup_im' src="${info_imgURL}"></img>`+
-    "<br>"+
-    `<p class='popup_title'>${info_titulo}</p>`+
-    `<p class='popup_title'>${info_acr}</p>`+
-    `<p class='popup_subtitle'>${info_pais}, ${info_city} (${info_year})</p>`+
-    "<br>"+
-    `<p class='popup_content'>Tipo de obra: ${info_tipoObra}</p>`+
-    `<p class='popup_content'>Usos: ${info_usos}</p>`+
-    `<p class='popup_content'>Presupuesto: ${info_presu} ${info_moneda}</p>`+
-    `<p class='popup_content'>Dimensión: ${info_dimension}</p>`+
-    `<p class='popup_content'>Autores: ${info_autores}</p>`+
-    "<br>"+
-    `<a href="${info_webURL}">Link a la obra completa</a>`
-
-    popup.setLngLat(e.lngLat).setHTML(popupContent).addTo(map);
-    // set this lot's polygon feature as the data for the highlight source
-    map.getSource('highlight-feature').setData(clickedFeature.geometry);
-    // show the cursor as a pointer
-    map.getCanvas().style.cursor = 'pointer';
-
-  } else {
-    //hide the popup if there are no features
-    popup.remove();
-  }
-  //console.log(features)
-});
